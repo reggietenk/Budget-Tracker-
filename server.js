02c237198f251fa67c6c04d432d3d8b77cfbb9ee
reggietenk <reggietenkorang@gmail.com>
2022-04-10 23:45:31 -0400

    added server function

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/budget-tracker', {
   useNewUrlParser: true,
-  useFindAndModify: false,
+  // useFindAndModify: false,
   family: 4
 });
 
