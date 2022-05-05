8f532b76da7467a9cd4a33887acf35b904330ffb
reggietenk <reggietenkorang@gmail.com>
2022-05-05 14:13:40 -0400

    fixed server connection

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/budget-tracker';
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/budget';
+;
 
 const app = express();
 
@@ -16,9 +17,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/budget-tracker', {
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
-  // useFindAndModify: false,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
   family: 4
 });
 
